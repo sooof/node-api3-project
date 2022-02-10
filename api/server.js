46b05a8a46b8cf5fec0537a17522edce7f5164db
sooof <sooof@me.com>
2022-02-09 17:27:42 -0800

    api test

--- api/server.js
+++ api/server.js
@@ -1,13 +1,30 @@
 const express = require('express');
+const usersRouter = require('./users/users-router')
 
 const server = express();
-
-// remember express by default cannot parse JSON in request bodies
+server.use(express.json())
 
 // global middlewares and the user's router need to be connected here
+
+server.use('/api/users', usersRouter)
+
+server.get('/hello', (req, res)=>{
+  res.json({message: "hello world!!!"})
+})
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+server.use('*', (req, res)=>{
+  res.status(404).json({message: `${req.method} ${req.baseUrl}, not found!!!`})
+})
+
+server.use((err, req, res, next)=>{
+  console.log('ERR')
+  res.status(err.status || 500).json({
+    message: `The Horror: ${err.message}`,
+  })
+})
 module.exports = server;
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -7,38 +7,46 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   // RETURN AN ARRAY WITH ALL THE USERS
+  res.json({message: "TEST: get /api/users !!!"})
 });
 
 router.get('/:id', (req, res) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
+  res.json({message: "TEST: get /api/users/:id !!!"})
 });
 
 router.post('/', (req, res) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
+  res.json({message: "TEST: post /api/users !!!"})
 });
 
 router.put('/:id', (req, res) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  res.json({message: "TEST: put /api/users/:id !!!"})
 });
 
 router.delete('/:id', (req, res) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
+  res.json({message: "TEST: delete /api/users/:id !!!"})
 });
 
 router.get('/:id/posts', (req, res) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
+  res.json({message: "TEST: get /api/users/:id/posts !!!"})
 });
 
 router.post('/:id/posts', (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  res.json({message: "TEST: post /api/users/:id/posts !!!"})
 });
 
 // do not forget to export the router
+module.exports = router
